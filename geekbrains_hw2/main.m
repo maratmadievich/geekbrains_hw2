e0645810079efa1fda8f4f6c9449eadbebe4d38a
Admin <admin@iMac-Admin.local>
2019-02-12 09:47:01 +0600

    Initial Commit

---
+++ geekbrains_hw2/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  geekbrains_hw2
+//
+//  Created by Admin on 2/12/19.
+//  Copyright © 2019 Admin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
