6ca0637794c3742099c1d4308505d797d8adc283
Admin <admin@iMac-Admin.local>
2019-02-12 09:54:22 +0600

    1) Сделано первое ДЗ

--- geekbrains_hw2/main.m
+++ geekbrains_hw2/main.m
@@ -7,11 +7,29 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "HomeworkOne.h"
+
+//int selectHomework() {
+//    int value = -1;
+//    printf("Выберите Домашнее задание:\n");
+//    printf("1-4 ДЗ;\n");
+//    printf("0 Выход.\n");
+//    scanf("%d", &value);
+//    printf("\n\n");
+//    return value;
+//}
+
+
 
 int main(int argc, const char * argv[]) {
+    
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+//        int selectedHomework = -1;
+        
+        HomeworkOne *homeworkOne = [[HomeworkOne alloc]init];
+        
+        [homeworkOne selectTask];
+        
     }
     return 0;
 }
