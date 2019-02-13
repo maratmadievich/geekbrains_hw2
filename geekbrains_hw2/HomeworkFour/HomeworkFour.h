bba3417d647f7ee7d22c3d239945dc3acf87c2f7
Admin <admin@iMac-Admin.local>
2019-02-13 14:41:45 +0600

    Домашки 2-4

---
+++ geekbrains_hw2/HomeworkFour/HomeworkFour.h
@@ -0,0 +1,17 @@
+//
+//  HomeworkFour.h
+//  geekbrains_hw2
+//
+//  Created by Admin on 2/13/19.
+//  Copyright © 2019 Admin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HomeworkFour : NSObject {
+    
+}
+
+-(void)showStudents;
+
+@end
